1128da78b82f5bc0508708e45c4cfbb91cdde7ca
Toggy <tognolamontes@yahoo.com.br>
2021-03-31 16:32:05 -0300

    add routes and Get rest

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+
+const userRoute = require('./routes/userRoute')
+
 const app = express()
 const port = 3000
 
+userRoute(app)
 
-app.listen(3000, () => console.log('Api rodando na porta 3000'))
+app.get('/', (req, res) => res.send('Olá mundo pelo Express'))
+
+app.listen(port, () => console.log('Api rodando na porta 3000'))
